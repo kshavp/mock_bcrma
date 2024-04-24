19523fdd3a11ab38e11104b4462c303832f68575
Keshav Priyam <keshavpriyam@gmail.com>
2024-04-25 04:54:08 +0530

    crud demo working

--- server/server.js
+++ server/server.js
@@ -35,3 +35,45 @@ app.post('/books',async(req,res)=>{
         return res.status(500).json(err);
     }
 });
+
+app.get('/books/:id',async(req,res)=>{
+    try{
+        const id = req.params.id;
+        const book = await bookModel.findById(id);
+        if(!book) return res.status(400).send("Book Not Found!");
+        return res.status(201).json(book);
+    }
+    catch(err){
+        return res.status(500).json(err);
+    }
+});
+
+app.put('/books/:id',async(req,res)=>{
+    try{
+        if(!req.body.title || !req.body.author || !req.body.yearOfPublish) return res.status(400).send("Fields are Missing");
+        const book = {
+            title : req.body.title,
+            author : req.body.author,
+            yearOfPublish : req.body.yearOfPublish
+        }
+        const id = req.params.id;
+        const bookFound = await bookModel.findByIdAndUpdate(id,book)
+        if(!bookFound) return res.status(404).send("Book Not Found");
+        return res.status(201).json("Book Updated");
+    }
+    catch(err){
+        return res.status(500).json(err);
+    }
+});
+
+app.delete('/books/:id',async(req,res)=>{
+    try{
+        const id = req.params.id;
+        const bookDeleted = bookModel.findByIdAndDelete(id);
+        if(!bookDeleted) return res.status(404).send("Book Not Found");
+        return res.status(201).json("Book Deleted");
+    }
+    catch(err){
+        return res.status(500).json(err);
+    }
+});
