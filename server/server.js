62ea33d396c68994bac09b805ee33676e0b8d493
Keshav Priyam <keshavpriyam@gmail.com>
2024-04-27 17:12:16 +0530

    add cors policy

--- server/server.js
+++ server/server.js
@@ -2,10 +2,17 @@ import express from 'express';
 import { PORT, mongoURL } from './config.js';
 import mongoose from 'mongoose';
 import router from './routes.js';
+import cors from 'cors';
 
 const app = express();
+
 app.use(express.json());
 app.use('/books', router);
+app.use(cors({
+    options : ['GET','POST','PUT','DELETE'],
+    origin : ['http://localhost:3000'],
+    allowedHeaders : ['content-type']
+}));
 
 app.get('/',(req,res)=>{
     return res.status(200).send("Running");
