0f7845c2646ed5c1113100fceae834da63c1f663
Keshav Priyam <keshavpriyam@gmail.com>
2024-04-29 09:34:51 +0530

    add pages

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1>Book Crud Management App</h1>
+    </div>
+  )
+}
+
+export default Home
---
+++ client/src/pages/ReadBook.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+const ReadBook = () => {
+  return (
+    <div>
+        <h1>Book Details</h1>
+    </div>
+  )
+}
+
+export default ReadBook
